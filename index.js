385c8bddbc647d9684cc2fbd05b344aedb1bacb6
Ekan <ekanessien@gmail.com>
2026-02-06 11:33:24 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 "use strict";
+
+const url = "quiz.json";
+const questions = [];
+const cur = 0; // current question
+
+window.addEventListener("DOMContentLoaded", () => {
+  console.log("ready");
+  loadQuestions();
+});
+
+function loadQuestions() {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    });
+}
